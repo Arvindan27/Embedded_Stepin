337dbe43ac0928b7db9d50c8eb0d4abc4f974fa7
Arvindan27 <arvindan.p.27@gmail.com>
2021-04-27 12:33:58 +0530

    program

--- project_main.c
+++ project_main.c
@@ -1,9 +1,9 @@
 /**
  * @file project_main.c
- * @author Bharath.G ()
+ * @author Arvindan.P()
  * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
  * @version 0.1
- * @date 2021-04-23
+ * @date 2021-04-25
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -14,40 +14,22 @@
 #include "blinky.h"
 
 /**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
-void peripheral_init(void)
-{
-	/* Configure LED Pin */
-	DDRB |= (1 << DDB0);
-}
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
-
-/**
  * @brief Main function where the code execution starts
  * 
  * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
+ * @note PORTD4 is in sink config. i.e when pin is Low, the LED will turn OFF
+ * @note PORTD4 is in sink config. i.e when pin is High, the LED will turn ON
  */
-int main(void)
-{
-	/* Initialize Peripherals */
-	peripheral_init();
+int main(void){
+    DDRD=DDRD|(1<<4);
+    DDRC=DDRC&~(1<<4);
+    DDRC=DDRC &~(1<<5);
+    while(1){
+        if((PINC & (1<<5))&&(PINC & (1<<4))){
+                PORTD=PORTD|(1<<4);
+            }else{
+            PORTD=PORTD &~(1<<4);
+        }
+    }
+	return 0;}
 
-	for(;;)
-	{
-        change_led_state(LED_ON);
-		delay_ms(LED_ON_TIME);
-		
-        change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
-	}
-	return 0;
-}
